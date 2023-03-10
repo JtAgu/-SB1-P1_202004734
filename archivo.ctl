1c896f0559d0f6ee03ae6c27c226c1df7e40e26b
Justin Aguirre <3016491460101@ingenieria.usac.edu.gt>
2023-03-10 00:50:26 -0600

    empezar a llenar

--- archivo.ctl
+++ archivo.ctl
@@ -1,3 +1,31 @@
-OPTIONS (SCTP+1)
-LOAD DATA
-CHARACTERSET UTF8
+OPTIONS (SKIP=1)
+    LOAD DATA
+    CHARACTERSET UTF8
+    INFILE 'DB_EXCEL.csv'
+    INTO TABLE temp TRUNCATE
+    FIELDS TERMINATED BY ";"
+    TRAILING NULLCOLS(
+        Nombre_Victima,
+        Apellido_Victima,
+        Direccion_Victima,
+        Fecha_Sospecha DATE 'DD-MM-YYYY HH24.MI',
+        Fecha_Confirmacion DATE 'DD-MM-YYYY HH24.MI',
+        Fecha_Muerte DATE 'DD-MM-YYYY HH24.MI',
+        Estado,
+        Nombre_Asociado,
+        Apellido_Asociado,
+        Fecha_Conocio DATE 'DD-MM-YYYY HH24.MI',
+        Contacto,
+        Fecha_Inicio_Contacto DATE 'DD-MM-YYYY HH24.MI',
+        Fecha_Final_Contacto DATE 'DD-MM-YYYY HH24.MI',
+        Nombre_Hospital,
+        Direccion_Hospital,
+        Ubicacion_Victima,
+        Fecha_Llegada DATE 'DD-MM-YYYY HH24.MI',
+        Fecha_Retiro DATE 'DD-MM-YYYY HH24.MI',
+        Tratamiento,
+        Efectividad,
+        Fecha_Inicio_Tratamiento DATE 'DD-MM-YYYY HH24.MI',
+        Fecha_Final_Tratamiento DATE 'DD-MM-YYYY HH24.MI',
+        Efectividad_Paciente
+    )
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+var exec = require("child_process");
+let connection = require('express-myconnection');
 const oracledb = require('oracledb');
 const app = express();
 const port = 3000;
@@ -11,36 +13,31 @@ async function selectAllEmployees(req, res) {
     connection = await oracledb.getConnection({
       user: "TEST",
       password: password,
-      connectString: "34.125.181.145:1521/ORCL18"
+      connectString: "34.16.130.59:1521/ORCL18"
     });
 
     console.log('connected to database');
     // run query to get all employees
-    result = await connection.execute(`SELECT * FROM employees`);
-
-  } catch (err) {
-    //send error message
-    return res.send(err.message);
-  } finally {
-    if (connection) {
-      try {
-        // Always close connections
-        await connection.close();
-        console.log('close connection success');
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
+    result = await connection.execute(`SELECT * FROM TEMP`);
     if (result.rows.length == 0) {
       //query return zero employees
+      await connection.close();
       return res.send('query send no rows');
     } else {
       //send all employees
+      await connection.close();
       return res.send(result.rows);
     }
-
-  }
+  } catch (err) {
+    //send error message
+    await connection.close();
+    return res.send(err.message);
+  } 
 }
+
+app.get('/consulta10', function (req, res) {
+  selectAllEmployees(req, res);
+})
 
 
 async function selectEmployeesById(req, res, id) {
@@ -48,7 +45,7 @@ async function selectEmployeesById(req, res, id) {
     connection = await oracledb.getConnection({
       user: "TEST",
       password: password,
-      connectString: "34.125.181.145:1521/ORCL18"
+      connectString: "34.16.130.59:1521/ORCL18"
     });
     // run query to get employee with employee_id
     result = await connection.execute(`INSERT `);
@@ -70,6 +67,7 @@ async function selectEmployeesById(req, res, id) {
       return res.send('query send no rows');
     } else {
       //send all employees
+      console.log(result.rows)
       return res.send(result.rows);
     }
   }
@@ -77,9 +75,7 @@ async function selectEmployeesById(req, res, id) {
 
 
 //get consulta mostrar info de hospital y fallecidos
-app.get('/consulta1', function (req, res) {
-  selectAllEmployees(req, res);
-})
+
 
 //get /victimas?efectividad>5 en transfuciones>
 app.get('/consulta2', function (req, res) {
@@ -125,16 +121,197 @@ app.get('/consulta10', function (req, res) {
   selectAllEmployees(req, res);
 })
 
+
+async function EliminarModelo(req, res) {
+  try {
+    connection = await oracledb.getConnection({
+      user: "TEST",
+      password: password,
+      connectString: "34.125.234.117:1521/ORCL18"
+    });
+
+    console.log('connected to database');
+    // run query to get all employees
+    await connection.execute(` DROP TABLE VICTIMA_UBICACION`);
+
+    await connection.execute(`DROP TABLE VICTIMA_TRATAMIENTO`)
+
+    await connection.execute(`DROP TABLE VICTIMA_ASOCIADO`)
+
+    await connection.execute(`DROP TABLE TRATAMIENTO`)
+
+    await connection.execute(`DROP TABLE UBICACION`)
+
+    await connection.execute(`DROP TABLE HOSPITAL`)
+
+    await connection.execute(`DROP TABLE ASOCIADO`)
+
+    await connection.execute(`DROP TABLE VICTIMA`)
+
+
+      return res.send('QUERY COMPLETE');
+    
+  } catch (err) {
+    //send error message
+    await connection.close();
+    return res.send(err.message);
+  } 
+}
+
+
 app.get('/eliminarModelo', function (req, res) {
-  selectAllEmployees(req, res);
+  EliminarModelo(req, res);
 })
+
+async function cargarTemp(req, res) {
+  try {
+    connection = await oracledb.getConnection({
+      user: "TEST",
+      password: password,
+      connectString: "34.125.234.117:1521/ORCL18"
+    });
+
+    //console.log('connected to database');
+    // run query to get all employees
+    result = await connection.execute(`CREATE TABLE TEMP (
+                                        Nombre_Victima VARCHAR2(50),
+                                        Apellido_Victima VARCHAR2(50),
+                                        Direccion_Victima VARCHAR2(100),
+                                        Fecha_Sospecha DATE,
+                                        Fecha_Confirmacion DATE,
+                                        Fecha_Muerte DATE,
+                                        Estado VARCHAR2(50),
+                                        Nombre_Asociado VARCHAR2(50),
+                                        Apellido_Asociado VARCHAR2(50),
+                                        Fecha_Conocio DATE,
+                                        Contacto VARCHAR2(50),
+                                        Fecha_Inicio_Contacto DATE,
+                                        Fecha_Final_Contacto DATE,
+                                        Nombre_Hospital VARCHAR2(50),
+                                        Direccion_Hospital VARCHAR2(100),
+                                        Ubicacion_Victima VARCHAR2(100),
+                                        Fecha_Llegada DATE,
+                                        Fecha_Retiro DATE,
+                                        Tratamiento VARCHAR2(50),
+                                        Efectividad INT,
+                                        Fecha_Inicio_Tratamiento DATE,
+                                        Fecha_Final_Tratamiento DATE,
+                                        Efectividad_Paciente INT
+                                        )`);
+      await connection.close();
+
+      exec.exec('sqlldr userid=TEST/1234@34.125.234.117:1521/ORCL18 control=archivo.ctl')
+      return res.send('QUERY COMPLETE');
+
+      
+    
+  } catch (err) {
+    //send error message
+    await connection.close();
+    return res.send(err.message);
+  } 
+}
 
 app.get('/cargarTemporal', function (req, res) {
-  selectAllEmployees(req, res);
+  cargarTemp(req, res);
 })
 
+
+
+
+
+async function cargarModelo(req, res) {
+  try {
+    connection = await oracledb.getConnection({
+      user: "TEST",
+      password: password,
+      connectString: "34.125.234.117:1521/ORCL18"
+    });
+
+    console.log('connected to database');
+    // run query to get all employees
+    await connection.execute(`CREATE TABLE VICTIMA (
+      Id_Victima INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+          Nombre_Victima VARCHAR2(50),
+          Apellido_Victima VARCHAR2(50),
+          Direccion_Victima VARCHAR2(100),
+          Fecha_Sospecha DATE,
+          Fecha_Confirmacion DATE,
+          Fecha_Muerte DATE,
+          Estado VARCHAR2(50)
+      )`)
+      
+      await connection.execute(`CREATE TABLE ASOCIADO (
+        Id_Asociado INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+          Nombre_Asociado VARCHAR2(50),
+          Apellido_Asociado VARCHAR2(50)
+      )`)
+      
+      await connection.execute(`CREATE TABLE HOSPITAL (
+          Id_Hospital INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+          Nombre_Hospital VARCHAR2(100),
+          Direccion_Hospital VARCHAR2(100)
+      )`)
+      
+      await connection.execute(`CREATE TABLE UBICACION (
+        Id_Ubicacion INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+        Ubicacion_Victima VARCHAR2(100)
+      )`)
+      
+      await connection.execute(`CREATE TABLE TRATAMIENTO (
+        Id_Tratamiento INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+          Tratamiento VARCHAR2(100),
+          Efectividad INT
+      )`)
+      
+      await connection.execute(`CREATE TABLE VICTIMA_TRATAMIENTO (
+        Id_Tratamiento INT,
+        CONSTRAINT fk_tratamiento_tratamiento FOREIGN KEY (Id_Tratamiento) REFERENCES TRATAMIENTO(Id_Tratamiento),
+        Id_Victima INT,
+        CONSTRAINT fk_victima_tratamiento FOREIGN KEY (Id_Victima) REFERENCES VICTIMA(Id_Victima),
+            Fecha_Inicio_Tratamiento DATE,
+            Fecha_Final_Tratamiento DATE,
+            Efectividad_Paciente INTEGER
+      )`)
+      
+      await connection.execute(`CREATE TABLE VICTIMA_ASOCIADO (
+        Id_Asociado INT,
+        CONSTRAINT fk_asociado_asociado FOREIGN KEY (Id_Asociado) REFERENCES ASOCIADO(Id_Asociado),
+        Id_Victima INT,
+        CONSTRAINT fk_victima_asociado FOREIGN KEY (Id_Victima) REFERENCES VICTIMA(Id_Victima),
+            Fecha_Conocio DATE,
+            Contacto VARCHAR2(50),
+            Fecha_Inicio_Contacto DATE,
+            Fecha_Final_Contacto DATE
+      )`)
+      
+      
+      await connection.execute(`CREATE TABLE VICTIMA_UBICACION (
+        Id_Ubicacion INT,
+        CONSTRAINT fk_ubicacion_ubicacion FOREIGN KEY (Id_Ubicacion) REFERENCES UBICACION(Id_Ubicacion),
+        Id_Victima INT,
+        CONSTRAINT fk_victima_ubicacion FOREIGN KEY (Id_Victima) REFERENCES VICTIMA(Id_Victima),
+            Fecha_Llegada DATE,
+            Fecha_Retiro DATE
+      )
+      `);
+
+      
+      await connection.execute(`INSERT INTO ASOCIADO  (Nombre_Asociado , Apellido_Asociado) SELECT DISTINCT Nombre_Asociado , Apellido_Asociado FROM TEMP`);
+
+      
+      await connection.close();
+      return res.send('QUERY COMPLETE');
+    
+  } catch (err) {
+    //send error message
+    await connection.close();
+    return res.send(err.message);
+  } 
+}
+
 app.get('/cargarModelo', function (req, res) {
-  selectAllEmployees(req, res);
+  cargarModelo(req, res);
 })
 
 
